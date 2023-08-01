a6f247b55753965c77ad4e53f0cffc506b0b19c4
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2023-07-31 20:49:05 -0500

    I will someday learn to save before committing

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -49,5 +49,36 @@
 (if false
   "By Odin's elbow")
 
-;; For `if`, each branch can only have one form
+;; For `if`, each branch can only have one form for truthy or falsey
 ;; To get around this limitation, we use the `do` operator
+(if true
+  (do (println "Success!")
+      "By zeus's hammer!")
+  (do (println "Failure!")
+      "By Aquaman's trident!"))
+
+;; The `when` operator is like a combo of `if` and `do` w/o an `else` branch
+(when true
+  (println "Success!")
+  "abra cadabra")
+
+;; Boolean operators `or` and `and`
+(or (= 0 1) (= "yes" "yes"))
+(or (= 0 1) (= 7 8) (= "yes" "yes"))
+(or (= 0 1) (= 7 8) (= "yes" "no"))
+(and (= 1 1) (= "yes" "yes"))
+(and (= 1 1) (= 8 8) (= "yes" "yes"))
+(and (= 1 1) (= 7 8) (= "yes" "no"))
+
+;; `or` returns either the first truthy value or the last value
+(or false nil :this :that)
+(or false nil)
+(or nil)
+
+;; `and` returns the first falsey value, or if no values are falsey, then it returns the last truthy value
+(and :free_wifi :hot_coffee)
+(and :free_wifi nil :hot_coffee)
+
+;; Use `def` to bind a name to a value in Clojure
+(def failed-protagonist-names
+  ["Larry Potter" "Doreen the Explorer" "The Incredible Bulk"])
